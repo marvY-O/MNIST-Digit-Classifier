d0b590b8e1bbe42305e972817bdd009ab2bd6ef4
Vyom Verma <vyomverma2003@gmail.com>
2022-12-25 21:53:01 +0530

    changed name and removed secret key

--- mnist_classifier/settings.py
+++ mnist_classifier/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 102400
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e%os)z*xf6qzfq268u=6=wikwmeuq(0+6y!e4*ymd(w^45$(j@'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER'
 
 ALLOWED_HOSTS = ['*']
 
